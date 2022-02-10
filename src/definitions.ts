0006d5feccef040e54af0e169723c11f4ef4f780
Yagnik031 <90082946+Yagnik031@users.noreply.github.com>
2022-02-10 17:47:34 +0530

    added

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,54 @@
+// flash light methods....
 export interface ScannerFlashPlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
+
+  // check if flashlight available
+  isAvailable(): Promise<{ value: boolean; }>;
+
+  // turn on flash-light
+  switchOn(options: {
+    intensity?: number;
+  }): Promise<void>;
+
+  // turn off flash-light
+  switchOff(): Promise<void>;
+
+  // check flashlight state
+  isSwitchedOn(): Promise<{ value: boolean; }>;
+
+
 }
+// end of flash-light methods....
+
+// start scanner methods....
+
+export interface BarcodeScannerPlugin {
+  prepare(): Promise<void>;
+  hideBackground(): Promise<void>;
+  showBackground(): Promise<void>;
+  startScan(): Promise<ScanResult>;
+  stopScan(): Promise<void>;
+  checkPermission(
+    options: CheckPermissionOptions,
+  ): Promise<CheckPermissionResult>;
+  openAppSettings(): Promise<void>;
+}
+
+export interface ScanResult {
+  hasContent: boolean;
+  content?: string;
+}
+
+export interface CheckPermissionOptions {
+  force?: boolean;
+}
+
+export interface CheckPermissionResult {
+  granted?: boolean;
+  denied?: boolean;
+  asked?: boolean;
+  neverAsked?: boolean;
+  restricted?: boolean; // ios only
+  unknown?: boolean; // ios only
+}
+
--- src/web.ts
+++ src/web.ts
@@ -1,8 +1,42 @@
 import { WebPlugin } from '@capacitor/core';
+import { BarcodeScannerPlugin, CheckPermissionOptions, CheckPermissionResult, ScanResult } from './definitions';
 
 import type { ScannerFlashPlugin } from './definitions';
 
-export class ScannerFlashWeb extends WebPlugin implements ScannerFlashPlugin {
+export class ScannerFlashWeb extends WebPlugin implements ScannerFlashPlugin, BarcodeScannerPlugin {
+  async prepare(): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  async hideBackground(): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  async showBackground(): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  async startScan(): Promise<ScanResult> {
+    throw new Error('Method not implemented.');
+  }
+  async stopScan(): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  async checkPermission(_options: CheckPermissionOptions): Promise<CheckPermissionResult> {
+    throw new Error('Method not implemented.');
+  }
+  async openAppSettings(): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  async isAvailable(): Promise<{ value: boolean; }> {
+    throw new Error('Method not implemented.');
+  }
+  async switchOn(_options: { intensity?: number | undefined; }): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  async switchOff(): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  async isSwitchedOn(): Promise<{ value: boolean; }> {
+    throw new Error('Method not implemented.');
+  }
   async echo(options: { value: string }): Promise<{ value: string }> {
     console.log('ECHO', options);
     return options;
