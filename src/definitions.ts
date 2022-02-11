4b8648fa5747f6fcaaebd3cf856844cbe4311f90
Yagnik031 <90082946+Yagnik031@users.noreply.github.com>
2022-02-11 11:32:50 +0530

    r

--- src/definitions.ts
+++ src/definitions.ts
@@ -37,6 +37,35 @@ export interface ScannerFlashPlugin {
 
 // }
 
+
+export declare enum SupportedFormat {
+  UPC_A = "UPC_A",
+  UPC_E = "UPC_E",
+  UPC_EAN_EXTENSION = "UPC_EAN_EXTENSION",
+  EAN_8 = "EAN_8",
+  EAN_13 = "EAN_13",
+  CODE_39 = "CODE_39",
+  CODE_39_MOD_43 = "CODE_39_MOD_43",
+  CODE_93 = "CODE_93",
+  CODE_128 = "CODE_128",
+  CODABAR = "CODABAR",
+  ITF = "ITF",
+  ITF_14 = "ITF_14",
+  AZTEC = "AZTEC",
+  DATA_MATRIX = "DATA_MATRIX",
+  MAXICODE = "MAXICODE",
+  PDF_417 = "PDF_417",
+  QR_CODE = "QR_CODE",
+  RSS_14 = "RSS_14",
+  RSS_EXPANDED = "RSS_EXPANDED"
+}
+
+
+export interface ScanOptions {
+  targetedFormats?: SupportedFormat[];
+}
+
+
 export interface ScanResult {
   hasContent: boolean;
   content?: string;
